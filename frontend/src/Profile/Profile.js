a73b9891cfac441310f9adb9d238106def7a8222
Dan <46987754+Monduli@users.noreply.github.com>
2021-09-18 15:07:53 -0400

    Updated Profile.js
    Added, then removed sidebar, made it match up with form.js more

--- frontend/src/Profile/Profile.js
+++ frontend/src/Profile/Profile.js
@@ -1,73 +1,98 @@
 import * as React from 'react';
 import "./Profile.css";
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import Button from '@mui/material/Button';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonIcon from '@mui/icons-material/Person';
+import AppBar from '@mui/material/AppBar';
+import Typography from '@mui/material/Typography';
+import Toolbar from '@mui/material/Toolbar';
+import CssBaseline from '@mui/material/CssBaseline';
 
-class Profile extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      mpg1: [{Miles_Driven: "", Miles_Per_Gallon: ""}]
-    };
-  }
+let drawerWidth = 240;
 
-  // The function for the button that adds more cars to the form
-  addClick(){
-    this.setState(prevState => ({
-      mpg1: [...prevState.mpg1, { Miles_Driven: "", Miles_Per_Gallon: ""}]
-    }))
-  }
+export default function Form() {
 
-  // The function that creates the HTML for the Vehicles section
-  createUI(){
-    return this.state.mpg1.map((el, i) => (
-      <div className = 'Profile' key={i+1}>
-        <h3>Car {i+1}</h3>
-        <h5>Current Emissions</h5>
-        <p>What is the average amount of miles driven by this vehicle per week?</p>
-        <input  type='number' placeholder="0" name="Miles_Driven" value={el.Miles_Driven||''} onChange={this.handleChange.bind(this, i)} />
-        <p>How many miles does this car get to the gallon?</p>
-        <input type='number' placeholder="0" name="Miles_Per_Gallon" value={el.Miles_Per_Gallon||''} onChange={this.handleChange.bind(this, i)} />
-        <p></p>
-        {/* <input type='button' value='Remove Vehicle' onClick={this.removeClick.bind(this, i)}/> */}
-      </div>
-    ))
-  }
-
-  // Code for the "Remove" button in Vehicles
-  removeClick(i){
-    let mpg1 = [...this.state.mpg1];
-    mpg1.splice(i, 1);
-    this.setState({ mpg1 })
-  }
-
-  // Change handler
-  handleChange(i, e) {
-    const { name, value } = e.target;
-    let mpg1 = [...this.state.mpg1];
-    mpg1[i] = {...mpg1[i], [name]: value};
-    this.setState({ mpg1 })
-  }
-
-  // The remainder of the HTML
-  render() {
-  return (
+  return (        
     <div className="Profile">
       <header className="Profile-header">
+      <CssBaseline />
+        <AppBar
+              position="fixed"
+              sx={{ width: `calc(100%)`, ml: `${drawerWidth}px` }}
+            >
+              <Toolbar>
+                <Typography variant="h6" noWrap component="div">
+                  Profile Information
+                </Typography>
+              </Toolbar>
+            </AppBar>
+        </header>
+        <body>
+        <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: 'background.default', p: 6 }}
+        >
         <h1>Gas Usage</h1>
         <form className="Profile-form" action="">
-            <p>Household's Primary Heating Source:</p>
-            <select name="Type_Heating" id="Type_Heating">
-            <option value="Natural_Gas">Natural Gas</option>
-            <option value="Electricity">Electricity</option>
-            <option value="Fuel_Oil">Fuel Oil</option>
-            <option value="Propane">Propane</option>
-            </select>
-            <p>How much does your usage cost per month?</p>
-            $<input type="number" id="Fuel_Use" name="Fuel_Use" min="0" placeholder="0"/>
+            <p>How much natural gas does your household use per month (dollars)?</p>
+                <input
+                type='number'
+                name='ng_usage'
+                placeholder='0'
+                />
+
+                <p>How much electricity does your household use per month (dollars)?</p>
+                <input
+                type='number'
+                name='electricity_usage'
+                placeholder='0'
+                />
+
+                <p>How much fuel oil does your household use per month?</p>
+                <input
+                type='number'
+                name='fuel_oil_usage'
+                placeholder='0'
+                />
+
+                <p>How much propane does your household use per month?</p>
+                <input
+                type='number'
+                name='pp_usage'
+                placeholder='0'
+                />
 
             <h1>Vehicles</h1>
 
-            {this.createUI()}
-            {/* <input type='button' value='Add Vehicle' onClick={this.addClick.bind(this)}/> */}
+            <h3>Car 1</h3>
+            <h5>Current Emissions</h5>
+            <p>What is the average amount of miles driven by this vehicle per week?</p>
+            <input  type='number' placeholder="0" name="Miles_Driven_1" placeholder="0"/>
+            <p>How many miles does this car get to the gallon?</p>
+            <input type='number' placeholder="0" name="Miles_Per_Gallon_1" placeholder="0"/>
+            <p></p>
+            <h3>Car 2</h3>
+            <h5>Current Emissions</h5>
+            <p>What is the average amount of miles driven by this vehicle per week?</p>
+            <input  type='number' placeholder="0" name="Miles_Driven_2" placeholder="0"/>
+            <p>How many miles does this car get to the gallon?</p>
+            <input type='number' placeholder="0" name="Miles_Per_Gallon_2" placeholder="0"/>
+            <p></p>
+            <h3>Car 3</h3>
+            <h5>Current Emissions</h5>
+            <p>What is the average amount of miles driven by this vehicle per week?</p>
+            <input  type='number' placeholder="0" name="Miles_Driven_3" placeholder="0"/>
+            <p>How many miles does this car get to the gallon?</p>
+            <input type='number' placeholder="0" name="Miles_Per_Gallon_3" placeholder="0"/>
+            <p></p>
+            {/*<input type='button' value='Add Vehicle' onClick={this.addClick.bind(this)}/>*/}
             <h3>Maintenance</h3>
             <p>Do you perform regular maintenance on your cars?</p>
             <select name="Vehicle_Maintenance" id="Vehicle_Maintenance">
@@ -111,9 +136,9 @@ class Profile extends React.Component {
         <footer>
           2021 BeaverHacks Fall Hackathon
         </footer>
-      </header>
+        </Box>
+      </body>
     </div>
   );
-}}
-
-export default Profile;
+  
+}
